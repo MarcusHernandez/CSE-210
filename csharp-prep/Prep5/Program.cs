4a1dea57624db24aa184281b350c4c69f977e82f
Marcus Hernandez <marcus.hernandez.10401@gmail.com>
2023-04-26 17:12:13 -0600

    Added function code using static.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,56 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+
+    static void WelcomeMessage()
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string InputNameLast()
+    {
+        Console.Write("Please enter your last name: ");
+        string namelast = Console.ReadLine();
+
+        return namelast;
+    }
+
+    static string InputNameFirst()
+    {
+        Console.Write("Plase enter your First Name: ");
+        string namefirst = Console.ReadLine();
+
+        return namefirst;
+    }
+    static int InputNum()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int NumberSquared(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void NameAndSquare(string namelast, string namefirst, int square)
+    {
+        Console.WriteLine($"{namelast}, {namefirst} {namelast}, the square of your number is {square}");
+    }
+
+        static void Main(string[] args)
+    {
+        WelcomeMessage();
+
+        string userNameLast = InputNameLast();
+        string userNameFirst = InputNameFirst();
+        int userNumber = InputNum();
+
+        int squaredNumber = NumberSquared(userNumber);
+
+        NameAndSquare(userNameLast, userNameFirst, squaredNumber);
     }
 }
