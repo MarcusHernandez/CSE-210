e7b1f3332c59497fb0d0196d5a5693948f037b53
Marcus Hernandez <marcus.hernandez.10401@gmail.com>
2023-04-26 16:24:46 -0600

    Added Code to create a Random number guesser

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //int magicnum=0;
+        //Console.Write("Please Enter a Magic Number: ");
+        //magicnum = int.Parse(Console.ReadLine());
+        Random randomnumber = new Random();
+        int magicnum = randomnumber.Next(1,500);
+        int guessnum = -1;
+    while (guessnum!=magicnum)
+    {
+        Console.Write("Please Enter your Guess: ");
+        guessnum = int.Parse(Console.ReadLine());
+            if (guessnum<magicnum)
+        {
+            Console.WriteLine("Higher");
+        }
+            else if (guessnum > magicnum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else{
+                Console.WriteLine("You Guessed Right!");
+            }
+    }
+
     }
 }
