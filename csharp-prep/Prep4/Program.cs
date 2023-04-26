29e5cbd8e89d2e1e7d7c0c09392b5273363acc6e
Marcus Hernandez <marcus.hernandez.10401@gmail.com>
2023-04-26 16:54:17 -0600

    Added Code to get the Sum, Average, and Max number From a List.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int InputNum= -1;
+        Console.WriteLine("Please Enter a Number, Type 0 to end.");
+        
+
+        while (InputNum !=0)
+        {
+            Console.Write("Please Enter a Number:");
+            //string Numtyped = Console.ReadLine();
+            //InputNum = int.Parse(Numtyped);
+            InputNum = int.Parse(Console.ReadLine());
+            if (InputNum!=0)
+            {
+                numbers.Add(InputNum);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum+=number;
+        }
+        Console.WriteLine($"The Sum is {sum}");
+
+        float average = ((float)sum)/numbers.Count;
+        Console.WriteLine($"The Average of the Numbers is {average}");
+
+        int maxnum = numbers[0];
+        foreach(int number in numbers)
+        {
+            if(number>maxnum)
+            {
+                maxnum=number;
+            }
+        }
+
+        Console.WriteLine($"The Biggest Number is {maxnum}");
     }
 }
