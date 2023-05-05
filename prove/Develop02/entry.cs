c01f84d91eda41e0502347e77d8325fd9fa7a251
Marcus Hernandez <marcus.hernandez.10401@gmail.com>
2023-05-05 13:52:14 -0600

    added a entry class and a prompt class

---
+++ prove/Develop02/entry.cs
@@ -0,0 +1,20 @@
+public class Entry
+{
+
+public entry()
+{
+
+}
+
+public string AddDate()
+{
+    return "Today's Date";
+}
+
+public string AddResponse()
+{
+    return "response";
+}
+
+
+}
---
+++ prove/Develop02/prompt.cs
@@ -0,0 +1,24 @@
+public class Prompt
+{
+    List<string> _prompts = new List<string>();
+
+    public Prompt()
+    {
+
+    }
+
+    public string PickRandomPrompt()
+    {
+        return "This is the Prompt";
+    }
+
+    public void AddPrompt()
+    {
+
+    }
+
+
+
+
+
+}
