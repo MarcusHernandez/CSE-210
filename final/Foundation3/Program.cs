2f2940861d84397a71242204d380599f96689c63
Marcus Hernandez <marcus.hernandez.10401@gmail.com>
2023-07-19 01:46:51 -0600

    Added a Way to clear the console after you were done.

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,17 +4,14 @@ class Program
 {
     static void Main()
     {
-        // Create addresses
-        Address lectureAddress = new Address("123 Main St", "New York", "NY", "USA");
-        Address receptionAddress = new Address("456 Park Ave", "Los Angeles", "CA", "USA");
-        Address gatheringAddress = new Address("789 Beach Rd", "Miami", "FL", "USA");
+        Address lectureAddress = new Address("123 Main St", "Idaho", "ID", "USA");
+        Address receptionAddress = new Address("17th Steet", "Los Angeles", "CA", "USA");
+        Address gatheringAddress = new Address("Sunnyside", "Salt lake City", "UT", "USA");
 
-        // Create events
         Lecture lectureEvent = new Lecture("Tech Talk", "An informative tech talk on Programming", DateTime.Parse("2023-07-25"), TimeSpan.Parse("14:00"), lectureAddress, "John Doe", 100);
-        Reception receptionEvent = new Reception("Networking Night", "An evening of networking and socializing", DateTime.Parse("2023-08-10"), TimeSpan.Parse("18:30"), receptionAddress, "rsvp@example.com");
-        OutdoorGathering gatheringEvent = new OutdoorGathering("Picnic in the Park", "A fun picnic at the park", DateTime.Parse("2023-09-05"), TimeSpan.Parse("12:00"), gatheringAddress, "Sunny with a high of 80°F");
+        Reception receptionEvent = new Reception("Networking Night", "An evening of networking and socializing", DateTime.Parse("2023-08-10"), TimeSpan.Parse("18:30"), receptionAddress, "rsvp@Gmail.com");
+        OutdoorGathering gatheringEvent = new OutdoorGathering("Picnic at the Zoo", "A fun picnic at the Zoo", DateTime.Parse("2023-09-05"), TimeSpan.Parse("12:00"), gatheringAddress, "Sunny with a high of 80°F");
 
-        // Generate marketing messages
         Console.WriteLine("Standard Details:");
         Console.WriteLine(lectureEvent.GetStandardDetails());
         Console.WriteLine(receptionEvent.GetStandardDetails());
@@ -29,5 +26,11 @@ class Program
         Console.WriteLine(lectureEvent.GetShortDescription());
         Console.WriteLine(receptionEvent.GetShortDescription());
         Console.WriteLine(gatheringEvent.GetShortDescription());
+
+        Console.WriteLine("\n\n\n");
+
+        Console.WriteLine("Press Enter to Close Data");
+        Console.ReadLine();
+        Console.Clear();
     }
 }
